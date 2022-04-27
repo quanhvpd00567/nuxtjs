23192cae39378374bbbfcbaf4f52c066ff52a2b2
Hoang Quan <quan.hv@neo-lab.vn>
2022-04-27 09:30:26 +0700

    css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-app',
+    title: 'Roasted Beef',
     htmlAttrs: {
       lang: 'en'
     },
